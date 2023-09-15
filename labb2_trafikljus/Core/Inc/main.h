c63e547ded765f9aa7c0f45ed61ad26b60e04d7a
AronKG <122611265+AronKG@users.noreply.github.com>
2023-09-15 16:23:25 +0200

    done trffic light labb

--- labb2_trafikljus/Core/Inc/main.h
+++ labb2_trafikljus/Core/Inc/main.h
@@ -31,7 +31,27 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+enum event {
+	ev_none,
+	ev_button_push,
+	ev_state_timeout,
+	ev_error = -99
+};
 
+
+enum state {
+	s_init,
+	s_both_stop,
+	s_gande_go,
+	sp_both_read,
+	s_car_ready,
+	s_car_go,
+	s_pushed_wait,
+	s_car_wait,
+	s_car_stopping
+};
+
+void my_systick_handler();
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
@@ -49,6 +69,8 @@ extern "C" {
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -59,12 +81,23 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define B1_EXTI_IRQn EXTI15_10_IRQn
+#define TL_A_Pin GPIO_PIN_0
+#define TL_A_GPIO_Port GPIOA
+#define TL_B_Pin GPIO_PIN_1
+#define TL_B_GPIO_Port GPIOA
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define TL_C_Pin GPIO_PIN_4
+#define TL_C_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define TL_D_Pin GPIO_PIN_7
+#define TL_D_GPIO_Port GPIOA
+#define TL_E_Pin GPIO_PIN_8
+#define TL_E_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
--- labb2_trafikljus/Core/Src/main.c
+++ labb2_trafikljus/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "abuzz.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -37,25 +37,175 @@
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
+
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+int systick_count=0;
+uint32_t tick_left_in_state =0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+#define EVQ_SIZE 10
+
+enum event evq[ EVQ_SIZE ];
+int evq_count = 0;
+int evq_front_ix = 0;
+int evq_rear_ix = 0;
+
+void evq_push_back(enum event e)
+{
+// if queue is full, ignore e
+	if ( evq_count < EVQ_SIZE )
+	{
+		evq[evq_rear_ix] = e;
+		evq_rear_ix++;
+		evq_rear_ix %= EVQ_SIZE;
+		evq_count++;
+	}
+}
+enum event evq_pop_front()
+{
+	enum event e = ev_none;
+	if ( evq_count > 0 )
+	  {
+		e = evq[evq_front_ix];
+		evq[evq_front_ix] = ev_error; // detect stupidity
+		evq_front_ix++;
+		evq_front_ix %= EVQ_SIZE;
+		evq_count--;
+	  }
+return e;
+}
+
+int is_blue_butten_pressed();
+
+void set_traffic_lights( enum state st){
+
+
+	switch(st){
+		case s_init:
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_SET);
+			break;
+		case s_both_stop:
+
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_RESET);
+			break;
+		case  s_gande_go:
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_SET);
+			break;
+		case sp_both_read:
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_RESET);
+			break;
+		case s_car_ready:
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_RESET);
+			break;
+		case s_car_go:
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_RESET);
+			break;
+		case s_pushed_wait:
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_RESET);
+			break;
+		case s_car_stopping:
+
+			HAL_GPIO_WritePin(TL_A_GPIO_Port, TL_A_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_B_GPIO_Port, TL_B_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_C_GPIO_Port, TL_C_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(TL_D_GPIO_Port, TL_D_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(TL_E_GPIO_Port, TL_E_Pin, GPIO_PIN_RESET);
+			break;
+		default:
+			break;
+
+	}
+}
+
+
+
+int is_blue_butten_pressed()
+{
+
+	if(GPIOC->IDR & (1 << 13)){
+		return 0;
+	}
+	else{
+	   return 1;
+	}
+}
+
+
+void evq_init()
+{
+	for(int i = 0; i < EVQ_SIZE; i++)
+	{
+		evq[i] = ev_error;
+	}
+}
+
+void HAL_GPIO_EXTI_Callback( uint16_t GPIO_Pin )
+{
+	if(GPIO_Pin == B1_Pin)
+	{
+		evq_push_back(ev_button_push);
+	}
+
+}
+
+void my_systick_handler() {
+
+
+	if(tick_left_in_state > 0){
+		tick_left_in_state--;
+		if(tick_left_in_state == 0){
+			//ev = ev_state_timeout;
+			evq_push_back(ev_state_timeout);
+		}
+	}
+}
 
 /* USER CODE END 0 */
 
@@ -88,14 +238,139 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  abuzz_start();
+  abuzz_p_long();
+
+  enum state st = s_init;
+  enum event ev = ev_none;
+
+
+
+ uint32_t curr_tick = 0;
+ uint32_t last_tick = 0;
+ int last_button_press = 0;
+
+  evq_init();
+
+
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+	// set_traffic_lights(st);
   while (1)
   {
+
+/*
+	 int curr_button_press = is_blue_butten_pressed();
+	  //last_tick = curr_tick;
+
+	  if (curr_button_press && ! last_button_press){
+		  //ev = ev_button_push;
+		   evq_push_back(ev_button_push);
+	  }
+
+	  last_button_press = curr_button_press;
+
+
+	 //evq_push_back(ev_button_push);
+
+	 //void HAL_GPIO_EXTI_Callback( uint16_t GPIO_Pin );
+
+
+	 curr_tick = HAL_GetTick();
+
+
+	 if (curr_tick != last_tick){
+		 if(tick_left_in_state > 0){
+			 tick_left_in_state--;
+			 if(tick_left_in_state == 0){
+				 //ev = ev_state_timeout;
+				 evq_push_back(ev_state_timeout);
+			 }
+		 }
+		 //last_tick = curr_tick;
+		 last_tick = curr_tick;
+
+	 }
+*/
+	  ev = evq_pop_front();
+
+	  switch(st){
+	  		case s_init:
+	  			set_traffic_lights(st);
+				if(ev == ev_button_push){
+					ev = ev_none;
+					tick_left_in_state = 1000;
+					st = s_both_stop;
+					set_traffic_lights(st);
+				}
+				break;
+	  		case s_both_stop:
+	  			if(ev == ev_state_timeout){
+	  			ev = ev_none;
+	  			st =s_gande_go;
+	  			tick_left_in_state = 2000;
+				set_traffic_lights(st);
+	  			}
+	  			break;
+	  		case s_gande_go:
+	  			if(ev == ev_state_timeout){
+	  			 ev = ev_none;
+	  			st= sp_both_read;
+	  			tick_left_in_state = 1000;
+				set_traffic_lights(st);
+	  			}
+	  			break;
+	  		case sp_both_read:
+	  			if(ev == ev_state_timeout){
+	  				ev = ev_none;
+					st = s_car_ready;
+		  			tick_left_in_state = 1000;
+					set_traffic_lights(st);
+	  			}
+	  			break;
+	  		case s_car_ready:
+	  			if(ev == ev_state_timeout){
+					ev = ev_none;
+					st = s_car_go;
+		  			tick_left_in_state = 2000;
+					set_traffic_lights(st);
+	  			}
+	  			break;
+			case s_car_go:
+	  			if(ev == ev_button_push){
+					ev = ev_none;
+					st = s_pushed_wait;
+					tick_left_in_state = 1000;
+					set_traffic_lights(st);
+	  			}
+				break;
+			case s_pushed_wait:
+				if(ev == ev_state_timeout){
+					ev = ev_none;
+					st = s_car_stopping;
+					tick_left_in_state = 1000;
+					set_traffic_lights(st);
+				}
+				break;
+			case s_car_stopping:
+				if(ev == ev_state_timeout){
+					ev = ev_none;
+					st = s_both_stop;
+					tick_left_in_state = 1000;
+					set_traffic_lights(st);
+				}
+				break;
+			default:
+				break;
+
+	  }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -150,6 +425,65 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 65535;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -200,7 +534,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, TL_A_Pin|TL_B_Pin|TL_C_Pin|LD2_Pin
+                          |TL_D_Pin|TL_E_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -208,12 +543,18 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : TL_A_Pin TL_B_Pin TL_C_Pin LD2_Pin
+                           TL_D_Pin TL_E_Pin */
+  GPIO_InitStruct.Pin = TL_A_Pin|TL_B_Pin|TL_C_Pin|LD2_Pin
+                          |TL_D_Pin|TL_E_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
